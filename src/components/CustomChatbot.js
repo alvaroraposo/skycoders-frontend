7c64f4b771262d183e2f19fc1918964ee0796201
alvaroraposo <alvaroraposo@yahoo.com.br>
2020-09-20 00:33:54 +0100

    um monte de coisa

--- src/components/CustomChatbot.js
+++ src/components/CustomChatbot.js
@@ -26,12 +26,10 @@ function ValidaMail ({id, triggerNextStep}) {
           triggerNextStep({trigger: '4'});
         }
         else {
-          console.log("else");
           triggerNextStep({trigger: '2'});
         }
 
         setLoading(!loading);        
-        console.log("message body", messageBody);
         setConnectionParams({
           messageBody
         });                  
@@ -51,7 +49,29 @@ function BuscaMensagens (props) {
   const [message, setMessage] = useState("");
   const [loading, setLoading] = useState(true);
 
+  useEffect(() => {    
+    const enviarMensagem = async () => {
+      console.log("previousValue", props.previousStep.message);
+      const headers = {
+        'Access-Control-Expose-Headers': 'Access-Control-Allow-Origin',
+        'Access-Control-Allow-Credentials': true,
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',            
+      }
+
+      const result = await axios.post(`https://ke1lzcm9le.execute-api.us-east-1.amazonaws.com/dev/send`, {
+        messageGroupId: props.id,
+        messageDeduplicationId: uuid.v1(),
+        messageBody: props.previousStep.message,
+      });
+    }
+    
+    if(props.previousStep && props.previousStep.message)
+      enviarMensagem();
+  }, [])
+
   useEffect(() => {
+    console.log("código!", "1061712315074-01");
     const intervalId = setInterval(async () => {
       const resConsultaMensagens = await axios.get(`https://876bvfo0j9.execute-api.us-east-1.amazonaws.com/dev/receive/${props.id}`);
       const consultaMensagensCode = resConsultaMensagens.status;
@@ -59,28 +79,30 @@ function BuscaMensagens (props) {
       if(consultaMensagensCode === 200) {
         const resMessagesWaiting = resConsultaMensagens.data.message.messagesWaiting;
         const messageBody = resConsultaMensagens.data.message.messageBody;
+        
+        console.log(messageBody, resMessagesWaiting);
 
         if(resMessagesWaiting < 0)
           return;
-        
+                
         clearInterval(intervalId);
 
         if(resMessagesWaiting === (messagesWaiting - 1) || messagesWaiting === -1){             
           setMessagesWaiting(resMessagesWaiting); 
-          setMessage(JSON.parse(messageBody).replaceAll("\"", ""));
+          setMessage(JSON.parse(messageBody));
           setLoading(!loading);
 
           if(resMessagesWaiting > 0) {                                    
-            //props.triggerNextStep({trigger: '2'});
-            props.triggerNextStep({trigger: '4'}); // temporario
+            props.triggerNextStep({trigger: '2'});
+            //props.triggerNextStep({trigger: '4'}); // temporario
           }
           else {
-            //props.triggerNextStep({trigger: '3'});
-            props.triggerNextStep({trigger: '4'});
+            props.triggerNextStep({trigger: '3'});
+            //props.triggerNextStep({trigger: '4'});
           }
         }
       }
-    }, 4000);
+    }, 2000);
     return () => {
       clearInterval(intervalId);
     }
@@ -90,6 +112,32 @@ function BuscaMensagens (props) {
   return (
     <span>{(!loading) ? message : "Um momento, por favor..."}</span>
   );
+}
+
+function EnviarMensagens(props) {
+    useEffect(() => {    
+    const enviarMensagem = async () => {
+      console.log("previousValue", props.previousStep.message);
+      const headers = {
+        'Access-Control-Expose-Headers': 'Access-Control-Allow-Origin',
+        'Access-Control-Allow-Credentials': true,
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',            
+      }
+
+      const result = await axios.post(`https://ke1lzcm9le.execute-api.us-east-1.amazonaws.com/dev/send`, {
+        messageGroupId: props.id,
+        messageDeduplicationId: uuid.v1(),
+        messageBody: props.previousStep.message,
+      });
+    }
+    
+    console.log("message - id", props.previousStep.message, props.id);
+    if(props.previousStep && props.previousStep.message)
+      enviarMensagem();
+  }, [])
+
+  return null;
 }
 
 function CustomChatbot ({id}) {
@@ -117,6 +165,12 @@ function CustomChatbot ({id}) {
         id: '4',
         message: 'Volte sempre!',
         end: true
+      },
+      {
+        id: '5',
+        component: <EnviarMensagens id={id}/>,
+        asMessage: true,
+        trigger: '2'
       }
     ]
   )
